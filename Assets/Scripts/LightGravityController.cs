4cadfdfbf81d511d470ba6b558ab6181d143293d
ArtemkaKun! <zumikq78@gmail.com>
2020-03-26 12:42:04 +0100

    Add: LightGravityController

---
+++ Assets/Scripts/LightGravityController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class LightGravityController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.GetComponent<Rigidbody2D>().gravityScale = 0.3f;
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        other.GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+}
