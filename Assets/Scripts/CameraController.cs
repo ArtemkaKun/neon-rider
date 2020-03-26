b7b9192b5444dfc868cf41ad905c87cce2412a81
ArtemkaKun! <zumikq78@gmail.com>
2020-03-26 11:01:45 +0100

    CarController && CameraController was added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject car;
+    void Update()
+    {
+        var car_position = car.transform.position;
+        transform.position = new Vector3(car_position.x, car_position.y, -10);
+    }
+}
---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Math.Abs(Input.GetAxis("Horizontal")) > 0)
+        {
+            var sphere = gameObject.GetComponent<WheelJoint2D>();
+            sphere.useMotor = true;
+            var joint_motor_2d = sphere.motor;
+            joint_motor_2d.motorSpeed += Input.GetAxis("Horizontal") * 75f * Time.fixedDeltaTime;
+            sphere.motor = joint_motor_2d;
+        }
+    }
+}
