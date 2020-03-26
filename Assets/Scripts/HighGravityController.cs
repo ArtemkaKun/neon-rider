4eb2aa4d6904749925ce46539b19ca9291487085
ArtemkaKun! <zumikq78@gmail.com>
2020-03-26 12:00:57 +0100

    Add: HighGravityController

---
+++ Assets/Scripts/HighGravityController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighGravityController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.GetComponent<Rigidbody2D>().gravityScale = 5;
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        other.GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+}
--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -1,16 +1,17 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class MoveController : MonoBehaviour
 {
-    private Rigidbody2D car_rigidbody;
+    private Rigidbody2D _carRigidbody;
 
     private void Start()
     {
-        car_rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        _carRigidbody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        car_rigidbody.AddTorque(-1 * Input.GetAxis("Horizontal") * 75f * Time.fixedDeltaTime);
+        _carRigidbody.AddTorque(-1 * Input.GetAxis("Horizontal") * 75f * Time.fixedDeltaTime);
     }
 }
